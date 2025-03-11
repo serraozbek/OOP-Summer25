69e9e1aa58e373a622d6808743c1a9542c8e546f
serraozbek <sserraozbek@gmail.com>
2025-03-11 10:19:35 +0000

    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,17 @@
+my_age = 19 #int
+
+my_weight = 58.5 #float
+
+my_name = "Serra" #str
+
+favorite_colors = ["pink", "blue", "yellow"] #lists
+
+personal_info = {"name": "serra", "age": 19} #dict
+
+colors = ("red", "green", "black") #tuples
+
+unique_numbers = {1, 2, 3, 4, 5}  # set
+
+is_student = True  # bool
+
+print(my_age, my_weight, my_name, favorite_colors, personal_info, colors, unique_numbers, is_student)
