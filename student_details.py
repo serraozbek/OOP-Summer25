b4e8cc0077080668ac93ba7f997eef5714aed554
serraozbek <sserraozbek@gmail.com>
2025-03-13 11:27:32 +0000

    Add Person class

---
+++ student_details.py
@@ -0,0 +1,14 @@
+first_name= "Serra"
+last_name= "Ozbek "
+index_number= 348
+nationality="Turkish"
+starting_date ="03-09-2024"
+courses =["math","computer science", "english"]
+
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Index Number:", index_number)
+print("Nationality:", nationality)
+print("Starting Date:", starting_date)
+print("Courses:", courses)
