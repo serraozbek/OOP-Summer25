af7f44478ab3788fdf1cb48c56efe917693481d9
serraozbek <sserraozbek@gmail.com>
2025-03-20 18:04:58 +0000

    Add Person class

---
+++ animal.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self, name, group, number_of_legs, skills):
+        self.name = name
+        self.group = group
+        self.number_of_legs = number_of_legs
+        self.skills = skills
+
+    def __str__(self):
+        return f"{self.name} ({self.group}): {self.number_of_legs} legs, Skills: {', '.join(self.skills)}"
+
+animals = [
+    Animal("Cat", "mammals", 4, ["jumping", "climbing", "sleeping"]),
+    Animal("Dog", "mammals", 4, ["running", "barking"]),
+    Animal("Fox", "mammals", 4, ["learning fast", "hunting"]),
+    Animal("Capybara", "mammals", 4, ["swimming and diving", "social behavior"]),
+    Animal("Snake", "reptiles", 0, ["slithering", "hunting"])
+]
+
+for animal in animals:
+    print(animal)
