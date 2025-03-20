fad312051ab8c5e9c045d0a30ccaccc67da7cf88
serraozbek <sserraozbek@gmail.com>
2025-03-20 11:10:11 +0000

    Add new file

---
+++ animals.py
@@ -0,0 +1,40 @@
+
+animal_1 = {
+    "name": "Cat",
+    "group": "mammals",
+    "number_of_legs": 4,
+    "skills": ["jumping", "climbing","sleeping"]
+}
+
+animal_2 = {
+    "name": "Dog",
+    "group": "mammals",
+    "number_of_legs": 4,
+    "skills": ["running", "barking"]
+}
+
+animal_3 = {
+    "name": "Fox",
+    "group": "mammals",
+    "number_of_legs": 4,
+    "skills": ["learning fast", "hunting"]
+}
+
+animal_4 = {
+    "name": "Capybara",
+    "group": "mammals",
+    "number_of_legs": 4,
+    "skills": ["swimming and diving", "ocial ehavior"]
+}
+
+animal_5 = {
+    "name": "Snake",
+    "group": "reptiles",
+    "number_of_legs": 0,
+    "skills": ["slithering", "hunting"]
+}
+
+animals = [animal_1, animal_2, animal_3, animal_4, animal_5]
+
+for animal in animals:
+    print(animal)
